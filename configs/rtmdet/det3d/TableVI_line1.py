9216dc86222b57f77a148aec2c7c7fcfd1bd26b8
贾译凇 <1725066940@qq.com>
2024-08-22 16:50:18 +0800

    Update TableVI_line1.py

--- configs/rtmdet/det3d/TableVI_line1.py
+++ configs/rtmdet/det3d/TableVI_line1.py
@@ -2,7 +2,7 @@ _base_ = './TableV_line6.py'
 
 work_dir = './work_dirs/TableVI_line1/'
 
-train_dataloader = dict(dataset=dict(ann_file='new/kitti_infos_trainval.pkl',),
+train_dataloader = dict(dataset=dict(ann_file='kitti_infos_trainval.pkl',),
                         drop_last=True
                         )
 train_cfg = dict(
