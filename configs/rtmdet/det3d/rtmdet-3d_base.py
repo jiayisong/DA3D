46643590b7d28ecffed2c3042045ff834b873fa4
贾译凇 <1725066940@qq.com>
2024-08-22 18:44:41 +0800

    Update rtmdet-3d_base.py

--- configs/rtmdet/det3d/rtmdet-3d_base.py
+++ configs/rtmdet/det3d/rtmdet-3d_base.py
@@ -1,8 +1,6 @@
 _base_ = '../../_base_/default_runtime.py'
 
-# checkpoint = 'https://download.openmmlab.com/mmdetection/v3.0/rtmdet/cspnext_rsb_pretrain/cspnext-l_8xb256-rsb-a1-600e_in1k-6a760974.pth'  # noqa
 
-# ========================Frequently modified parameters======================
 
 
 class_names = ['Pedestrian', 'Cyclist', 'Car']
