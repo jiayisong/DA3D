c530df458d171a8165db9bdbbbb76849552bcfc7
贾译凇 <1725066940@qq.com>
2024-08-22 16:33:14 +0800

    Update and rename v2-99_2xb4-100e_kitti3d.py to TableVI_line1.py

---
+++ configs/rtmdet/det3d/TableVI_line1.py
@@ -0,0 +1,14 @@
+_base_ = './TableV_line6.py'
+
+work_dir = './work_dirs/TableVI_line1/'
+
+train_dataloader = dict(dataset=dict(ann_file='new/kitti_infos_trainval.pkl',),
+                        drop_last=True
+                        )
+train_cfg = dict(
+    val_interval=5000,
+    dynamic_intervals=None,
+)
+test_evaluator = dict(
+    submission_prefix=work_dir + 'result/',
+)
